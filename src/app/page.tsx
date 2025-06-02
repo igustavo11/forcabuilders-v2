ddbce5dda5f79b733557702d4134579ed1c52eb8
Fernanda <fernanvipo7@gmail.com>
2025-06-01 22:57:17 -0300

    fix: social buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,21 +91,30 @@ export default function Home() {
           <h3 className="text-2xl font-bold mb-2">
               See more on our social channels
           </h3>
-          <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-6 mt-6">
-            <Link href="https://www.youtube.com/@forcabuilders"
-                  target="_blank"
-                  className="flex items-center bg-red-600 hover:bg-red-700 text-white font-semibold py-3 px-6 rounded-md transition-colors duration-300">
-              <Youtube size={25} />
-                YOUTUBE
+          <div className="flex flex-col md:flex-row justify-center items-center gap-4 mt-6">
+            <Link
+              href="https://www.youtube.com/@forcabuilders"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative flex items-center justify-center w-[200px] pl-6 pr-12 py-2.5 rounded-lg bg-gradient-to-tr from-red-600 to-red-500 text-white text-base font-bold uppercase shadow-md hover:shadow-xl transition-all duration-300 text-center"
+            >
+              Youtube
+              <span className="absolute right-0 h-full w-10 rounded-r-lg grid place-items-center bg-red-700">
+                <Youtube size={25}/>
+              </span>
             </Link>
 
-            <Link href="https://www.instagram.com/forcabuilders/"
-                  target="_blank"
-                  className="flex items-center bg-pink-600 hover:bg-pink-700 text-white font-semibold py-3 px-6 rounded-md transition-colors duration-300">
-              <Instagram size={25} />
-                INSTAGRAM
+            <Link
+              href="https://www.instagram.com/forcabuilders/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative flex items-center justify-center w-[200px] pl-6 pr-12 py-2.5 rounded-lg bg-gradient-to-tr from-pink-600 to-pink-500 text-white text-base font-bold uppercase shadow-md hover:shadow-xl transition-all duration-300 text-center"
+            >
+              Instagram
+              <span className="absolute right-0 h-full w-10 rounded-r-lg grid place-items-center bg-pink-700">
+                <Instagram size={25} />
+              </span>
             </Link>
-
           </div>
 
         </div>
